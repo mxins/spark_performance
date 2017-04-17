0325e151900b35a1f4f7d9e4917b9e9fd1e826d7
EC2 Default User <ec2-user@office-logserver.us-east.shinezoneserver.com>
2017-04-17 06:39:22 +0000

    spark udf

--- java/src/com/followeye/test/JavaSparkSQL.java
+++ java/src/com/followeye/test/JavaSparkSQL.java
@@ -2,15 +2,20 @@ package com.followeye.test;
 
 import org.apache.spark.SparkContext;
 import org.apache.spark.sql.*;
+import org.apache.spark.sql.api.java.UDF1;
 import org.apache.spark.sql.types.DataTypes;
 import org.apache.spark.sql.types.StructField;
 import org.apache.spark.sql.types.StructType;
+
+import java.time.LocalDate;
+import java.sql.Date;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by mxins@qq.com on 2017/3/21.
- * usage: spark-submit --class com.followeye.test.JavaWordCount java/out/artifacts/test_jar/test.jar
+ * usage: spark-submit --class com.followeye.test.JavaSparkSQL java/out/artifacts/test_jar/test.jar
  */
 public class JavaSparkSQL {
     static String baseDir = System.getProperty("user.dir");
@@ -36,6 +41,7 @@ public class JavaSparkSQL {
     }
 
     private static Dataset<Row> getGameDim(SparkSession spark) {
+        long startTime = System.currentTimeMillis();
         String path = baseDir + "/data/game_dim/";
 
         // The Schema is encoded in a string
@@ -55,10 +61,19 @@ public class JavaSparkSQL {
         // Creates a temporary view using the DataFrame
         gameDataFrame.createOrReplaceTempView("game_dim");
 
+        long tempTime = System.currentTimeMillis();
+        gameDataFrame.cache();
+        long count = gameDataFrame.count();
+        long endTime = System.currentTimeMillis();
+        System.out.println(String.format("table: %s, count: %d, save df cost time: %d seconds, " +
+                "count cost time: %d seconds, total: %d seconds.\n", "game_dim", count, tempTime - startTime,
+                endTime - tempTime, endTime - startTime));
+
         return gameDataFrame;
     }
 
     private static Dataset<Row> getConsumeLog(SparkSession spark) {
+        long startTime = System.currentTimeMillis();
         String path = baseDir + "/data/consume_log/";
 
         List<StructField> fields = new ArrayList<>();
@@ -77,10 +92,19 @@ public class JavaSparkSQL {
 
         Dataset<Row> consumeDataFrame = spark.read().schema(schema).csv(path);
         consumeDataFrame.createOrReplaceTempView("consume_log");
+
+        long tempTime = System.currentTimeMillis();
+        consumeDataFrame.cache();
+        long count = consumeDataFrame.count();
+        long endTime = System.currentTimeMillis();
+        System.out.println(String.format("table: %s, count: %d, save df cost time: %d seconds, " +
+                        "count cost time: %d seconds, total: %d seconds.\n", "consume_log", count,
+                tempTime - startTime, endTime - tempTime, endTime - startTime));
         return consumeDataFrame;
     }
 
     private static Dataset<Row> getConsumeAgg(SparkSession spark) {
+        long startTime = System.currentTimeMillis();
         String sql = "SELECT t1.game_id,\n" +
                 "       t1.server_id,\n" +
                 "       t1.uid,\n" +
@@ -99,10 +123,18 @@ public class JavaSparkSQL {
         Dataset<Row> consumeDataFrame = spark.sql(sql);
         consumeDataFrame.cache();
         saveDataFrame(consumeDataFrame, "consume_agg");
+
+        long tempTime = System.currentTimeMillis();
+        long count = consumeDataFrame.count();
+        long endTime = System.currentTimeMillis();
+        System.out.println(String.format("table: %s, count: %d, save df cost time: %d seconds, " +
+                        "count cost time: %d seconds, total: %d seconds.\n", "consume_agg", count,
+                tempTime - startTime, endTime - tempTime, endTime - startTime));
         return consumeDataFrame;
     }
 
     private static Dataset<Row> getConsumeAggRaw(SparkSession spark) {
+        long startTime = System.currentTimeMillis();
         String sql = "SELECT t.game_id,\n" +
                 "       t.server_id,\n" +
                 "       t.uid, \n" +
@@ -129,18 +161,45 @@ public class JavaSparkSQL {
         Dataset<Row> consumeDataFrameRaw = spark.sql(sql);
         consumeDataFrameRaw.cache();
         saveDataFrame(consumeDataFrameRaw, "consume_agg_raw");
+
+        long tempTime = System.currentTimeMillis();
+        long count = consumeDataFrameRaw.count();
+        long endTime = System.currentTimeMillis();
+        System.out.println(String.format("table: %s, count: %d, save df cost time: %d seconds, " +
+                        "count cost time: %d seconds, total: %d seconds.\n", "consume_agg_raw", count,
+                tempTime - startTime, endTime - tempTime, endTime - startTime));
         return consumeDataFrameRaw;
     }
 
+    class MonthDate implements UDF1<LocalDate, Integer> {
+//        private static final long serialVersionUID = 1L;
+
+        @Override
+        public Integer call(LocalDate s) throws Exception {
+            Integer month = s.getMonthValue();
+            return month;
+        }
+    }
+
+    class YearDate implements UDF1<LocalDate, Integer> {
+//        private static final long serialVersionUID = 1L;
+
+        @Override
+        public Integer call(LocalDate s) throws Exception {
+            Integer year = s.getYear();
+            return year;
+        }
+    }
+
     private static Dataset<Row> getConsumeAggUdf(SparkSession spark) {
-        SparkContext sparkContext = spark.sparkContext();
-        SQLContext sqlContext = SQLContext.getOrCreate(sparkContext);
-//        sqlContext.udf().registerJava("udf_year", );
+        long startTime = System.currentTimeMillis();
+        spark.udf().register("udf_year", (Date date) -> date.getYear(), DataTypes.IntegerType);
+        spark.udf().register("udf_month", (Date date) -> date.getMonth(), DataTypes.IntegerType);
         String sql = "SELECT t.game_id,\n" +
                 "       t.server_id,\n" +
                 "       t.uid, \n" +
-                "       year(register_date) AS date_year, \n" +
-                "       month(date_desc) AS date_month,\n" +
+                "       udf_year(register_date) AS date_year, \n" +
+                "       udf_month(date_desc) AS date_month,\n" +
                 "       t.level, \n" +
                 "       t.amount, \n" +
                 "       t.consume_times \n" +
@@ -162,6 +221,12 @@ public class JavaSparkSQL {
         Dataset<Row> consumeDataFrameUdf = spark.sql(sql);
         consumeDataFrameUdf.cache();
         saveDataFrame(consumeDataFrameUdf, "consume_agg_udf");
+        long tempTime = System.currentTimeMillis();
+        long count = consumeDataFrameUdf.count();
+        long endTime = System.currentTimeMillis();
+        System.out.println(String.format("table: %s, count: %d, save df cost time: %d seconds, " +
+                        "count cost time: %d seconds, total: %d seconds.\n", "consume_agg_udf", count,
+                tempTime - startTime, endTime - tempTime, endTime - startTime));
         return consumeDataFrameUdf;
     }
 }
